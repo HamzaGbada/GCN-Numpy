694e04e6e311c6c36029a0646292d5c68297c9e0
HamzaGbada <hamza.gbada@gmail.com>
2024-03-13 14:38:18 +0100

    gcn

--- GCN_scratch/model.py
+++ GCN_scratch/model.py
@@ -19,38 +19,10 @@ class GCN:
         return GraphUtils.softmax(self.out_layer.forward(H, A))
 
     def backward(self, y, y_hat, X: np.ndarray, A: np.ndarray, alpha=0.01):
-        loss = GraphUtils.loss_function(y, y_hat)
-        feat = np.dot(X.T, A.T)
-        error = y_hat - y
-        gradient_w = np.dot(feat, error)/A.shape[0]
-        gradient_b = np.dot(feat, error)/A.shape[0]
-        self.init_layer.weight = self.init_layer.weight - alpha * gradient
-        self.hidden_layer.weight = self.init_layer.weight - alpha * gradient
-        self.out_layer.weight = self.init_layer.weight - alpha * gradient
+        error = (y_hat - y) / y.shape[0]
+        gradient_out = self.out_layer.backward(error, alpha)
+        gradient_hid = self.hidden_layer.backward(error, alpha)
+        gradient_init = self.init_layer.backward(error, alpha)
 
-    def backward(self, X, A, H1, Y, probs):
-        # Compute gradient of loss w.r.t. logits
-        dL_dlogits = probs - Y
+        return gradient_init, gradient_hid, gradient_out
 
-        # Gradient of loss w.r.t. W2 and b2
-        dW2 = np.dot(H1.T, dL_dlogits)
-        db2 = np.sum(dL_dlogits, axis=0, keepdims=True)
-
-        # Update weights of output layer
-        self.W2 -= self.learning_rate * dW2
-        self.b2 -= self.learning_rate * db2
-
-        # Gradient of loss w.r.t. H1
-        dL_dH1 = np.dot(dL_dlogits, self.W2.T)
-
-        # Gradient of loss w.r.t. H1 activation
-        dH1_dZ1 = np.where(H1 > 0, 1, 0)
-
-        # Gradient of loss w.r.t. W1 and b1
-        dZ1_dW1 = np.dot(np.dot(X.T, A.T), dH1_dZ1)
-        dW1 = np.dot(X.T, np.dot(A.T, dL_dH1 * dH1_dZ1))
-        db1 = np.sum(dL_dH1 * dH1_dZ1, axis=0, keepdims=True)
-
-        # Update weights of hidden layer
-        self.W1 -= self.learning_rate * dW1
-        self.b1 -= self.learning_rate * db1

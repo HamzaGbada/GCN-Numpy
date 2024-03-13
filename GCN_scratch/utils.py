f5e1af678ad0b920c86ab0d2d3121d8a8027930c
HamzaGbada <hamza.gbada@gmail.com>
2024-03-13 14:06:00 +0100

    gcn

--- GCN_scratch/utils.py
+++ GCN_scratch/utils.py
@@ -19,14 +19,30 @@ class GraphUtils:
         return np.identity(A.shape[0]) - np.dot(d, np.dot(A, d))
 
     @staticmethod
-    def ReLU(x):
+    def ReLU(x: np.ndarray) -> np.ndarray:
         return x * (x > 0)
 
     @staticmethod
-    def softmax(x):
+    def softmax(x: np.ndarray) -> np.ndarray:
         """Compute softmax values for each sets of scores in x."""
         exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
         return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+    @staticmethod
+    def loss_function(y: np.ndarray, y_hat: np.ndarray) -> np.float_:
+        """
+        Compute the cross-entropy loss.
+
+            Args:
+            Y (numpy.ndarray): True labels, shape (N, C)
+            Y_hat (numpy.ndarray): Predicted probabilities, shape (N, C)
+
+            Returns:
+            float: Cross-entropy loss
+        """
+        return -np.sum(np.dot(y, np.log(y_hat))) / y.shape[0]
+
+
+
 
 if __name__ == "__main__":
     adjacency_matrix = np.array([

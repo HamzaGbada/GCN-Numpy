18a1d7136ae6d2d1c102a0b965f85bc37efd6741
HamzaGbada <hamza.gbada@gmail.com>
2024-03-13 15:16:11 +0100

    gcn

--- train.py
+++ train.py
@@ -1,19 +1,33 @@
 import numpy as np
 from dgl.data import CoraGraphDataset
+from torch_geometric.datasets import KarateClub, Planetoid
 
 from GCN_scratch.model import GCN
 from GCN_scratch.utils import GraphUtils
 
 if __name__ == "__main__":
-    dataset = CoraGraphDataset()
-    g = dataset[0]
-    A = g.adjacency_matrix()
-    X = g.ndata["feat"]
-    y = g.ndata['label']
+    dataset = Planetoid(root='data/Cora', name='Cora')
+
+    # Get the data
+    data = dataset[0]
+
+    # Extract the adjacency matrix
+    adj_matrix = np.zeros((data.num_nodes, data.num_nodes))
+    edge_index = data.edge_index.numpy()
+    adj_matrix[edge_index[0], edge_index[1]] = 1
+    adj_matrix[edge_index[1], edge_index[0]] = 1
+
+    # Convert to a sparse matrix
+    A = adj_matrix
+    print(f"the adjancy matrix {A}")
+    X = data.x
+    print(f"the node feature {X.shape}, the number of node {data.num_nodes}")
+    y = data.y
+    print(f"the node feature {y}, the number of node {data.num_nodes}")
 
     input_dim = X.shape[1]
     hidden_dim = 16  # Choose the size of the hidden layer
-    output_dim = y.shape[1]
+    output_dim = np.unique(y).shape[0]
     epochs = 10
     lr = 0.1
 

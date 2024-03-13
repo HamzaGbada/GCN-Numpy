dc11071fa101065a0ffdec0305bea12a78ac2a12
HamzaGbada <hamza.gbada@gmail.com>
2024-03-13 13:48:01 +0100

    gcn layer

--- GCN_scratch/layers.py
+++ GCN_scratch/layers.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+from GCN_scratch.utils import GraphUtils
+
 
 class GCNLayer:
     def __init__(self, in_feat, out_feat):
         self.weight = np.random.randn(in_feat, out_feat)
 
     def forward(self, X: np.ndarray, A: np.ndarray):
-        return np.relu
+        L = GraphUtils.normalized_graph_laplacien(A)
+        return GraphUtils.ReLU(np.dot(np.dot(L, X), self.weight))
+
